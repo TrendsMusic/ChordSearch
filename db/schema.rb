5f5bebe776153011de6b977cd4171b1cae69a64a
Tsubasa <aile104fujikawa@gmail.com>
2021-05-25 06:23:11 +0000

    herokuver3

--- db/schema.rb
+++ db/schema.rb
@@ -2,17 +2,45 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_171939) do
+ActiveRecord::Schema.define(version: 2021_05_25_062205) do
 
-  create_table "chords", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", charset: "utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", charset: "utf8", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "chords", charset: "utf8", force: :cascade do |t|
     t.string "section_numbar"
     t.string "content"
     t.bigint "song_id"
@@ -22,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_05_23_171939) do
     t.index ["song_id"], name: "index_chords_on_song_id"
   end
 
-  create_table "fields", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "fields", charset: "utf8", force: :cascade do |t|
     t.string "part1"
     t.string "part2"
     t.string "part3"
@@ -34,7 +62,7 @@ ActiveRecord::Schema.define(version: 2021_05_23_171939) do
     t.index ["chord_id"], name: "index_fields_on_chord_id"
   end
 
-  create_table "songs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "songs", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.string "artist"
     t.string "genre"
@@ -44,7 +72,7 @@ ActiveRecord::Schema.define(version: 2021_05_23_171939) do
     t.index ["user_id"], name: "index_songs_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
@@ -52,6 +80,8 @@ ActiveRecord::Schema.define(version: 2021_05_23_171939) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "chords", "songs"
   add_foreign_key "fields", "chords"
   add_foreign_key "songs", "users"
